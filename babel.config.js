d86f76bc9a46d1889d9933fe7e387e7d6e36ec96
Kent Ng <kent.ng201@gmail.com>
2024-10-20 17:29:19 +0800

    setup pocketto environment

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,18 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+    presets: [
+        'module:@react-native/babel-preset',
+    ],
+    plugins: [
+        ["@babel/plugin-proposal-decorators", { "version": "2023-11" }],
+        ["@babel/plugin-transform-class-static-block"],
+        [
+            "module-resolver",
+            {
+                alias: {
+                    // This needs to be mirrored in tsconfig.json
+                    src: "./src",
+                },
+            },
+        ],
+    ]
 };
--- index.js
+++ index.js
@@ -1,9 +1,40 @@
-/**
- * @format
- */
-
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
 
+import 'react-native-get-random-values';
+import PouchDB from 'pouchdb';
+import SQLiteAdapterFactory from 'pouchdb-adapter-react-native-sqlite';
+import SQLite from 'react-native-sqlite-2';
+import { DatabaseManager, p } from "pocketto";
+
+// setup for the SQL Lite Adapter for PouchDB
+const SQLiteAdapter = SQLiteAdapterFactory(SQLite);
+PouchDB.plugin(SQLiteAdapter);
+
+// polyfill for base64 encoding/decoding
+if (typeof btoa === 'undefined') global.btoa = base64.encode;
+if (typeof atob === 'undefined') global.atob = base64.decode;
+
+p.setEnvironment("react-native");
+p.setIdMethod('timestamp');
+DatabaseManager.connect('default', {
+    adapter: 'react-native-sqlite',
+    dbName: 'default'
+}).then((localDb) => {
+    const remoteHost = Platform.OS === 'android' ? 'http://192.168.68.107:5984' : 'http://localhost:5984';
+    DatabaseManager.connect(`${remoteHost}/test`, {
+        dbName: 'remote',
+        auth: {
+            username: 'admin',
+            password: 'qwer1234',
+        },
+    }).then((remoteDb) => {
+        localDb.sync(remoteDb, {
+            live: true,
+            retry: true,
+        });
+        p.setRealtime(true);
+    });
+});
 AppRegistry.registerComponent(appName, () => App);
